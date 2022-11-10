ad689cdc61e56173a85690952b7b530426ec7291
Dunham Perry <thedmp@me.com>
2022-11-10 10:36:01 -0700

    Finished Talk To Locals functionality

--- HowNowBrownCow/Map/LocationTypes/TravelCenter.java
+++ HowNowBrownCow/Map/LocationTypes/TravelCenter.java
@@ -1,11 +1,19 @@
 package HowNowBrownCow.Map.LocationTypes;
 
+import java.util.ArrayList;
+
 import HowNowBrownCow.Map.City;
 import HowNowBrownCow.Map.Location;
+import HowNowBrownCow.Transport.Transport;
 
 public class TravelCenter extends Location{
+    ArrayList<Transport> transportOptions;
+
     public TravelCenter(String name, City city){
         super(name, city);
     }
     
+    public void setTransportOptions(ArrayList<Transport> transportOptions) {
+        this.transportOptions = transportOptions;
+    }
 }
--- HowNowBrownCow/Player.java
+++ HowNowBrownCow/Player.java
@@ -1,8 +1,10 @@
 package HowNowBrownCow;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import HowNowBrownCow.LanguageSkill.LanguageSkillAbstract;
+import HowNowBrownCow.LanguageSkill.ModerateSkill;
 import HowNowBrownCow.LanguageSkill.NoSkill;
 import HowNowBrownCow.Map.Location;
 
@@ -82,14 +84,54 @@ public class Player {
         }
     }
 
-    public void talkToLocals(Location location){
-        String localLanguage = location.city.country.language;
+    public String[] generateGibberishArray(String[] gibberishArray) {
+        Random rnd = new Random();
+
+        for(int i = 0; i < gibberishArray.length; i++) {
+            String word = gibberishArray[i];
+            char[] charArray = word.toCharArray();
+            for(int j = 0; j < charArray.length; j++) {
+                charArray[j] = (char) (rnd.nextInt(26) + 'A');
+            }
+            gibberishArray[i] = new String(charArray);
+        }
+        return gibberishArray;
+    }
+
+    public String talkToLocals(){
+        setLanguageSkill("Spanish", new ModerateSkill());
+        String localLanguage = currentLocation.city.country.language;
         LanguageSkillAbstract languageSkill = getLanguageSkill(localLanguage);
 
+        Collectible collectible = new Collectible("Mona Lisa", "This is a test hint to see how translation works");
+        String[] hintArray = collectible.hint.split("\\s+");
+        String[] gibberishArray = generateGibberishArray(collectible.hint.split("\\s+"));
+        String finalHint = "";
+
+        double translationRate;
+        if (languageSkill instanceof NoSkill) {
+            translationRate = 0;
+        }
+        else if (languageSkill instanceof ModerateSkill) {
+            translationRate = 0.5;
+        }
+        else {
+            translationRate = 1;
+        }
+
+        for(int i = 0; i < hintArray.length; i++) {
+            if (Math.random() <= translationRate) {
+                finalHint = finalHint + hintArray[i] + " ";
+            } else {
+                finalHint = finalHint + gibberishArray[i] + " ";
+            }
+        }
+        
+        return finalHint;
     }
 
     public Collectible searchLandmark(){
         // Placeholder
-        return new Collectible();
+        return new Collectible("Test", "Test");
     }
 }
